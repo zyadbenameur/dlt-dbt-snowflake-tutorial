fffe8458d1f5b0ba897e98fc9122d41a35d47f80
zyadben <51174702+zyadbenameur@users.noreply.github.com>
2024-09-03 03:48:50 +0000

    renamed models in datamart

---
+++ dbt_project/models/datamart/.legacy_customer_orders.sql
@@ -0,0 +1,93 @@
+select
+    raw_orders.id as order_id,
+    raw_orders.user_id as customer_id,
+    customer.last_name as surname,
+    customer.first_name as givenname,
+    first_order_date,
+    order_count,
+    total_lifetime_value,
+    round(amount / 100.0, 2) as order_value_dollars,
+    raw_orders.status as order_status,
+-- payments.status as payment_status
+from airbyte_database.airbyte_schema.raw_orders as orders
+
+join
+    (
+        select first_name || ' ' || last_name as name, *
+        from airbyte_database.airbyte_schema.raw_customers
+    ) customer
+    on raw_orders.user_id = customer.sid
+
+join
+    (
+        select
+            b.id as customer_id,
+            b.name as full_name,
+            b.last_name as surname,
+            b.first_name as givenname,
+            min(order_date) as first_order_date,
+            min(
+                case
+                    when a.status not in ('returned', 'return_pending') then order_date
+                end
+            ) as first_non_returned_order_date,
+            max(
+                case
+                    when a.status not in ('returned', 'return_pending') then order_date
+                end
+            ) as most_recent_non_returned_order_date,
+            coalesce(max(user_order_seq), 0) as order_count,
+            coalesce(
+                count(case when a.status != 'returned' then 1 end), 0
+            ) as non_returned_order_count,
+            sum(
+                case
+                    when a.status not in ('returned', 'return_pending')
+                    then round(c.amount / 100.0, 2)
+                    else 0
+                end
+            ) as total_lifetime_value,
+            sum(
+                case
+                    when a.status not in ('returned', 'return_pending')
+                    then round(c.amount / 100.0, 2)
+                    else 0
+                end
+            ) / nullif(
+                count(
+                    case when a.status not in ('returned', 'return_pending') then 1 end
+                ),
+                0
+            ) as avg_non_returned_order_value,
+            array_agg(distinct a.id) as order_ids
+
+        from
+            (
+                select
+                    row_number() over (
+                        partition by user_id order by order_date, id
+                    ) as user_order_seq,
+                    *
+                from airbyte_database.airbyte_schema.orders
+            ) a
+
+        join
+            (
+                select first_name || ' ' || last_name as name, *
+                from airbyte_database.airbyte_schema.customers
+            ) b
+            on a.user_id = b.id
+
+        left outer join airbyte_database.stripe.payment c on a.id = c.orderid
+
+        where a.status not in ('pending') and c.status != 'fail'
+
+        group by b.id, b.name, b.last_name, b.first_name
+
+    ) customer_order_history
+    on raw_orders.user_id = customer_order_history.customer_id
+
+left outer join
+    airbyte_database.stripe.payment payments on raw_orders.id = payments.orderid
+
+where payments.status != 'fail'
